8164c58713bae56ea28f8aafc90e09ea3ea532c5
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-05-18 10:38:14 +0300

    changes

--- app/src/main/java/com/pixelplex/qtum/ui/activity/MainActivity/MainActivityPresenterImpl.java
+++ app/src/main/java/com/pixelplex/qtum/ui/activity/MainActivity/MainActivityPresenterImpl.java
@@ -245,3 +245,19 @@ class MainActivityPresenterImpl extends BasePresenterImpl implements MainActivit
         return mAmountForSendAction;
     }
 }
+
+    git filter-branch -f --env-filter '
+        OLD_EMAIL="therightwaystudio@gmail.com"
+        CORRECT_NAME="Kirill Volkov"
+        CORRECT_EMAIL="kirill.volkov.by@gmail.com"
+        if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+        then
+        export GIT_COMMITTER_NAME="$CORRECT_NAME"
+        export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+        fi
+        if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+        then
+        export GIT_AUTHOR_NAME="$CORRECT_NAME"
+        export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+        fi
+        ' --tag-name-filter cat -- --branches --tags
