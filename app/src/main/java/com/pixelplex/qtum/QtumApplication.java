a5f72be696800877b75015ac126329c771458410
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-05-18 11:44:37 +0300

    merge_reqv_improovments

--- app/src/main/java/com/pixelplex/qtum/QtumApplication.java
+++ app/src/main/java/com/pixelplex/qtum/QtumApplication.java
@@ -3,7 +3,10 @@ package com.pixelplex.qtum;
 import android.app.Application;
 import android.support.multidex.MultiDexApplication;
 
+import com.crashlytics.android.Crashlytics;
 import com.pixelplex.qtum.utils.FontManager;
+
+import io.fabric.sdk.android.*;
 
 /**
  * Created by kirillvolkov on 16.05.17.
@@ -14,6 +17,11 @@ public class QtumApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if(!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
+
         FontManager.init(getAssets());
     }
 }
