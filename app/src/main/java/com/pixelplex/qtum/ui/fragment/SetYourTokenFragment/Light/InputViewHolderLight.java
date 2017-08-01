fd303dbf07f94dff8317ee57a01165950d6c6d74
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-08-01 14:26:14 +0300

    green_btn_selector

--- app/src/main/java/com/pixelplex/qtum/ui/fragment/SetYourTokenFragment/Light/InputViewHolderLight.java
+++ app/src/main/java/com/pixelplex/qtum/ui/fragment/SetYourTokenFragment/Light/InputViewHolderLight.java
@@ -16,6 +16,6 @@ public class InputViewHolderLight extends InputViewHolder {
     public InputViewHolderLight(View itemView, OnValidateParamsListener listener) {
         super(itemView, listener);
         tilParam.setTypeface(FontManager.getInstance().getFont(tilParam.getContext().getString(R.string.proximaNovaRegular)));
-        etParam.setTypeface(FontManager.getInstance().getFont(etParam.getContext().getString(R.string.proximaNovaRegular)));
+        etParam.setTypeface(FontManager.getInstance().getFont(etParam.getContext().getString(R.string.proximaNovaSemibold)));
     }
 }
--- app/src/main/java/com/pixelplex/qtum/ui/fragment/WatchContractFragment/Light/WatchContractFragmentLight.java
+++ app/src/main/java/com/pixelplex/qtum/ui/fragment/WatchContractFragment/Light/WatchContractFragmentLight.java
@@ -5,6 +5,7 @@ import com.pixelplex.qtum.model.ContractTemplate;
 import com.pixelplex.qtum.ui.fragment.WatchContractFragment.OnTemplateClickListener;
 import com.pixelplex.qtum.ui.fragment.WatchContractFragment.TemplatesAdapter;
 import com.pixelplex.qtum.ui.fragment.WatchContractFragment.WatchContractFragment;
+import com.pixelplex.qtum.utils.FontManager;
 
 import java.util.List;
 
@@ -17,6 +18,17 @@ public class WatchContractFragmentLight extends WatchContractFragment {
     @Override
     protected int getLayout() {
         return R.layout.fragment_watch_contract_light;
+    }
+
+    @Override
+    public void initializeViews() {
+        super.initializeViews();
+
+        mTilContractName.setTypeface(FontManager.getInstance().getFont(getResources().getString(R.string.proximaNovaRegular)));
+        mTilContractAddress.setTypeface(FontManager.getInstance().getFont(getResources().getString(R.string.proximaNovaRegular)));
+
+        mEditTextContractName.setTypeface(FontManager.getInstance().getFont(getResources().getString(R.string.proximaNovaSemibold)));
+        mEditTextContractAddress.setTypeface(FontManager.getInstance().getFont(getResources().getString(R.string.proximaNovaSemibold)));
     }
 
     @Override
