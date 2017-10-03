b1da4856a1c87f56ab972052c2f785dbf6d1a49a
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-10-03 12:53:15 +0300

    Unit testing: initial

---
+++ app/src/test/java/org/qtum/wallet/SendPresenterTest.java
@@ -0,0 +1,23 @@
+package org.qtum.wallet;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Created by drevnitskaya on 03.10.17.
+ */
+
+public class SendPresenterTest {
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
