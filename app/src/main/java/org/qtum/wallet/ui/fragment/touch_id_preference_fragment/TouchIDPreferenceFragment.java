2820012dbfe4f846691ae46aa95c7ce709052767
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-10-06 13:13:34 +0300

    TouchID screen:architecture fixes;

--- app/src/main/java/org/qtum/wallet/ui/fragment/touch_id_preference_fragment/TouchIDPreferenceFragment.java
+++ app/src/main/java/org/qtum/wallet/ui/fragment/touch_id_preference_fragment/TouchIDPreferenceFragment.java
@@ -4,40 +4,50 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 
+import org.qtum.wallet.ui.fragment.backup_wallet_fragment.BackUpWalletFragment;
+import org.qtum.wallet.ui.fragment.wallet_main_fragment.WalletMainFragment;
 import org.qtum.wallet.ui.fragment_factory.Factory;
 import org.qtum.wallet.ui.base.base_fragment.BaseFragment;
 
 import butterknife.OnClick;
 
 
-public abstract class TouchIDPreferenceFragment extends BaseFragment implements TouchIDPreferenceFragmentView {
+public abstract class TouchIDPreferenceFragment extends BaseFragment implements TouchIDPreferenceView {
 
     private static final String IS_IMPORTING = "is_importing";
     private static final String PIN = "pin";
     private boolean mIsImporting;
 
     @OnClick({org.qtum.wallet.R.id.bt_enable_touch_id, org.qtum.wallet.R.id.bt_not_now})
-    public void onClick(View view){
-        switch (view.getId()){
+    public void onClick(View view) {
+        switch (view.getId()) {
             case org.qtum.wallet.R.id.bt_enable_touch_id:
                 getPresenter().onEnableTouchIdClick();
             case org.qtum.wallet.R.id.bt_not_now:
-                getPresenter().onNextScreen(mIsImporting);
+                if (mIsImporting) {
+                    WalletMainFragment walletFragment = WalletMainFragment.newInstance(getContext());
+                    getMainActivity().setRootFragment(walletFragment);
+                    openRootFragment(walletFragment);
+                } else {
+                    BaseFragment backUpWalletFragment = BackUpWalletFragment.newInstance(getContext(), true, getPin());
+                    openFragment(backUpWalletFragment);
+                }
+
                 break;
         }
     }
 
     public static BaseFragment newInstance(Context context, boolean isImporting, String pin) {
-        
+
         Bundle args = new Bundle();
-        args.putBoolean(IS_IMPORTING,isImporting);
-        args.putString(PIN,pin);
+        args.putBoolean(IS_IMPORTING, isImporting);
+        args.putString(PIN, pin);
         BaseFragment fragment = Factory.instantiateFragment(context, TouchIDPreferenceFragment.class);
         fragment.setArguments(args);
         return fragment;
     }
 
-    private TouchIDPreferenceFragmentPresenter mTouchIDPreferenceFragmentPresenter;
+    private TouchIDPreferencePresenter mTouchIDPreferencePresenterImpl;
 
     @Override
     public void initializeViews() {
@@ -47,12 +57,12 @@ public abstract class TouchIDPreferenceFragment extends BaseFragment implements 
 
     @Override
     protected void createPresenter() {
-        mTouchIDPreferenceFragmentPresenter = new TouchIDPreferenceFragmentPresenter(this);
+        mTouchIDPreferencePresenterImpl = new TouchIDPreferencePresenterImpl(this, new TouchIDInterractorImpl(getContext()));
     }
 
     @Override
-    protected TouchIDPreferenceFragmentPresenter getPresenter() {
-        return mTouchIDPreferenceFragmentPresenter;
+    protected TouchIDPreferencePresenter getPresenter() {
+        return mTouchIDPreferencePresenterImpl;
     }
 
     @Override
