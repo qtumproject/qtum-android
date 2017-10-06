43347438b1928e5fb824466358951663479e0527
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-10-06 14:27:41 +0300

    TouchID screen: presenter test;

--- app/src/main/java/org/qtum/wallet/ui/fragment/touch_id_preference_fragment/TouchIDPreferencePresenterImpl.java
+++ app/src/main/java/org/qtum/wallet/ui/fragment/touch_id_preference_fragment/TouchIDPreferencePresenterImpl.java
@@ -2,12 +2,12 @@ package org.qtum.wallet.ui.fragment.touch_id_preference_fragment;
 
 import org.qtum.wallet.ui.base.base_fragment.BaseFragmentPresenterImpl;
 
-class TouchIDPreferencePresenterImpl extends BaseFragmentPresenterImpl implements TouchIDPreferencePresenter {
+public class TouchIDPreferencePresenterImpl extends BaseFragmentPresenterImpl implements TouchIDPreferencePresenter {
 
     private TouchIDPreferenceView mTouchIDPreferenceView;
     private TouchIDInterractor mTouchIDPreferenceInteractor;
 
-    TouchIDPreferencePresenterImpl(TouchIDPreferenceView view, TouchIDInterractor interractor) {
+    public TouchIDPreferencePresenterImpl(TouchIDPreferenceView view, TouchIDInterractor interractor) {
         mTouchIDPreferenceView = view;
         mTouchIDPreferenceInteractor = interractor;
     }
