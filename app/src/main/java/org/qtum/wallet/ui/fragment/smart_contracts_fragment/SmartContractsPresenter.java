51724ded4e33ec821b03479e2b08634a97295eeb
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-10-09 12:27:00 +0300

    Smart Contracts screen: architecture reworking;

---
+++ app/src/main/java/org/qtum/wallet/ui/fragment/smart_contracts_fragment/SmartContractsPresenter.java
@@ -0,0 +1,7 @@
+package org.qtum.wallet.ui.fragment.smart_contracts_fragment;
+
+
+import org.qtum.wallet.ui.base.base_fragment.BaseFragmentPresenter;
+
+interface SmartContractsPresenter extends BaseFragmentPresenter {
+}
--- app/src/main/java/org/qtum/wallet/ui/fragment/smart_contracts_fragment/SmartContractsView.java
+++ app/src/main/java/org/qtum/wallet/ui/fragment/smart_contracts_fragment/SmartContractsView.java
@@ -3,5 +3,5 @@ package org.qtum.wallet.ui.fragment.smart_contracts_fragment;
 import org.qtum.wallet.ui.base.base_fragment.BaseFragmentView;
 
 
-public interface SmartContractsFragmentView extends BaseFragmentView {
+public interface SmartContractsView extends BaseFragmentView {
 }
