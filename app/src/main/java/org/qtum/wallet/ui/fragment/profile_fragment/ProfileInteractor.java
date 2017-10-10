c4dfc36b25422a41df0ffcf768864e19cca58556
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-10-10 16:13:08 +0300

    Profile screen: architecture reworking;

---
+++ app/src/main/java/org/qtum/wallet/ui/fragment/profile_fragment/ProfileInteractor.java
@@ -0,0 +1,15 @@
+package org.qtum.wallet.ui.fragment.profile_fragment;
+
+
+import org.qtum.wallet.datastorage.listeners.LanguageChangeListener;
+
+public interface ProfileInteractor {
+    void clearWallet();
+
+    void setupLanguageChangeListener(LanguageChangeListener listener);
+
+    void removeLanguageListener(LanguageChangeListener listener);
+
+    boolean isTouchIdEnable();
+
+}
--- app/src/main/java/org/qtum/wallet/ui/fragment/profile_fragment/ProfileView.java
+++ app/src/main/java/org/qtum/wallet/ui/fragment/profile_fragment/ProfileView.java
@@ -3,7 +3,8 @@ package org.qtum.wallet.ui.fragment.profile_fragment;
 
 import org.qtum.wallet.ui.base.base_fragment.BaseFragmentView;
 
-public interface ProfileFragmentView extends BaseFragmentView {
+public interface ProfileView extends BaseFragmentView {
     void startDialogFragmentForResult();
+
     void resetText();
 }
