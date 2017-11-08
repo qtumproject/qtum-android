d18abe0d2cb807bdebf4c3f0b01c53dd335cbc55
Kirill Volkov <kirill.volkov.by@gmail.com>
2017-11-08 12:35:54 +0300

    balance_big_number

--- app/src/main/java/org/qtum/wallet/utils/FontTextView.java
+++ app/src/main/java/org/qtum/wallet/utils/FontTextView.java
@@ -2,15 +2,26 @@ package org.qtum.wallet.utils;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.ViewTreeObserver;
 
 import org.qtum.wallet.R;
 
+import java.util.logging.Handler;
+
 
 public class FontTextView extends android.support.v7.widget.AppCompatTextView {
+
+    Typeface tf;
+
+    public Typeface getTypeface() {
+        return tf;
+    }
+
     public FontTextView(Context context) {
         this(context, null);
     }
@@ -31,7 +42,7 @@ public class FontTextView extends android.support.v7.widget.AppCompatTextView {
             String fontAsset = ta.getString(R.styleable.FontTextView_typefaceAsset);
 
             if (!TextUtils.isEmpty(fontAsset)) {
-                Typeface tf = FontManager.getInstance().getFont(fontAsset);
+                tf = FontManager.getInstance().getFont(fontAsset);
                 int style = Typeface.NORMAL;
 
                 if (getTypeface() != null)
@@ -43,6 +54,42 @@ public class FontTextView extends android.support.v7.widget.AppCompatTextView {
                     Log.d("FontText", String.format("Could not create a font from asset: %s", fontAsset));
             }
         }
+    }
+
+    public void setLongNumberText(final String number) {
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                if (allowWidthNumber(number)) {
+                    setText(number);
+                } else {
+                    setText(ContractBuilder.getShortBigNumberRepresentation(number));
+                }
+            }
+        });
+    }
+
+    public void setLongNumberText(final String number, int allowWidth) {
+        if (allowWidthNumber(number, allowWidth)) {
+            setText(number);
+        } else {
+            setText(ContractBuilder.getShortBigNumberRepresentation(number));
+        }
+    }
+
+    private boolean allowWidthNumber(String number) {
+        Paint p = new Paint();
+        p.setTypeface(getTypeface()); // if custom font use `TypeFace.createFromFile`
+        p.setTextSize(getTextSize());
+        return p.measureText(number) < getWidth();
+    }
+
+    private boolean allowWidthNumber(String number, int allowWidth) {
+        Paint p = new Paint();
+        p.setTypeface(getTypeface()); // if custom font use `TypeFace.createFromFile`
+        p.setTextSize(getTextSize());
+        return p.measureText(number) < allowWidth;
     }
 
 }
