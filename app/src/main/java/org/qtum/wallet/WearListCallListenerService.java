a115ab73cd3809d794335c094a24a27bfb0f8b89
Kirill Volkov <kirill.volkov.by@gmail.com>
2018-02-08 10:49:22 +0300

    wear_tesrt

--- app/src/main/java/org/qtum/wallet/WearListCallListenerService.java
+++ app/src/main/java/org/qtum/wallet/WearListCallListenerService.java
@@ -1,46 +1,26 @@
 package org.qtum.wallet;
 
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableListenerService;
 import com.google.gson.Gson;
-
-import org.qtum.wallet.dataprovider.services.update_service.UpdateService;
-import org.qtum.wallet.dataprovider.services.update_service.WatchUpdateService;
-import org.qtum.wallet.dataprovider.services.update_service.listeners.BalanceChangeListener;
 import org.qtum.wallet.datastorage.QtumSharedPreference;
 import org.qtum.wallet.datastorage.TinyDB;
 import org.qtum.wallet.model.gson.history.History;
-import org.qtum.wallet.model.gson.history.HistoryResponse;
-import org.qtum.wallet.ui.activity.main_activity.MainActivity;
-
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-
-import rx.Subscriber;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by kirillvolkov on 21.11.2017.
@@ -57,7 +37,6 @@ public class WearListCallListenerService extends WearableListenerService impleme
 
     private GoogleApiClient mApiClient;
     ServiceConnection mServiceConnection;
-    WatchUpdateService mUpdateService;
     List<String> addresses;
     String currentAddress;
 
@@ -73,11 +52,7 @@ public class WearListCallListenerService extends WearableListenerService impleme
         if(messageEvent.getPath().contains("/get_history")) {
             addresses = getPublicAddresses();
             currentAddress = QtumSharedPreference.getInstance().getCurrentAddress(getApplicationContext());
-            if (QtumApplication.instance == null || QtumApplication.instance.getWearableMessagingProvider() == null) {
-                upSocketService();
-            } else {
-                sendData();
-            }
+            sendData();
         } else {
             stopSelf();
         }
@@ -130,30 +105,6 @@ public class WearListCallListenerService extends WearableListenerService impleme
     private List<String> getPublicAddresses() {
         TinyDB tinyDB = new TinyDB(getApplicationContext());
         return tinyDB.getPublicAddresses();
-    }
-
-    private void upSocketService() {
-        mServiceConnection = new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                if (iBinder instanceof WatchUpdateService.WatchUpdateBinder) {
-                    mUpdateService = ((WatchUpdateService.WatchUpdateBinder) iBinder).getService();
-                    mUpdateService.clearNotification();
-                    mUpdateService.initGoogleApiClient(addresses, currentAddress);
-                }
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName componentName) {
-
-            }
-        };
-
-        Intent intent = new Intent(this, WatchUpdateService.class);
-        startForegroundService(intent);
-
-        bindService(new Intent(this,
-                WatchUpdateService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
     }
 
     private void sendData(String items, String balance, String uncBalance, String address) {
